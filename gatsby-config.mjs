6fcd1a392c037cccb54c91647e79bf48194dca4b
junisara <junisara@gmail.com>
2024-05-17 18:58:19 +0900

    algolia 추가 잔재 삭제함

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -187,28 +187,6 @@ const config = {
         display: `minimal-ui`,
       },
     },
-    {
-      // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.ALGOLIA_APP_ID,
-        // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
-        // Tip: use Search API key with GATSBY_ prefix to access the service from within components
-        apiKey: process.env.ALGOLIA_API_KEY,
-        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
-        queries,
-        chunkSize: 10000, // default: 1000
-        settings: {
-          // optional, any index settings
-          // Note: by supplying settings, you will overwrite all existing settings on the index
-        },
-        mergeSettings: false, // optional, defaults to false. See notes on mergeSettings below
-        concurrentQueries: false, // default: true
-        dryRun: false, // default: false, only calculate which objects would be indexed, but do not push to Algolia
-        continueOnFailure: false, // default: false, don't fail the build if Algolia indexing fails
-        algoliasearchOptions: undefined, // default: { timeouts: { connect: 1, read: 30, write: 30 } }, pass any different options to the algoliasearch constructor
-      },
-    },
   ],
 }
 
